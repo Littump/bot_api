c4fc5a07122eb043ab0eaa30fdd57a9cac815dc5
Max <maks.igitov.2004@mail.ru>
2023-11-07 04:00:37 +0300

    обновление

--- FSMFillForm.py
+++ FSMFillForm.py
@@ -2,7 +2,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class FSMFillForm(StatesGroup):
-    adress = State()
+    address = State()
     house_material = State()
     object_type = State()
     cnt_rooms = State()
